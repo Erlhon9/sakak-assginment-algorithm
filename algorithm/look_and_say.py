82bebfc09b613b60f8290ac7fdae5a5198f52fe2
Erlhon9 <hongsbus@gmail.com>
2024-03-19 11:12:44 +0900

    feat: set accuracy test

---
+++ algorithm/look_and_say.py
@@ -0,0 +1,3 @@
+def look_and_say_sequence(n):
+    return 1
+    
--- algorithm/tests.py
+++ algorithm/tests.py
@@ -1,2 +1,9 @@
-def test():
-    assert 1 == 1
+from look_and_say import look_and_say_sequence
+import pytest
+
+@pytest.mark.parametrize("n, result", [
+    (3, 21),(4, 21),(5, 12),(6, 22),(7, 12),(8, 21),
+])
+def test_accuracy(n, result):
+    assert look_and_say_sequence(n) == result
+    
