a1404bb1f9f3c66c78d795558556c2a120efa011
Erlhon9 <hongsbus@gmail.com>
2024-03-19 11:34:55 +0900

    feat: solution1 - inefficiency way

--- algorithm/look_and_say.py
+++ algorithm/look_and_say.py
@@ -1,3 +1,27 @@
 def look_and_say_sequence(n):
-    return 1
+    if n == 1:
+        return "1"
     
+    else:
+        before = list(look_and_say_sequence(n-1))
+        answer = ""
+        queue = []
+        while before != []:
+            num = before.pop(0)
+            if queue == []:
+                queue.append(num)
+            elif queue[-1] == num:
+                queue.append(num)
+            else:
+                answer += str(len(queue)) + queue[0]
+                queue = [num]
+        
+        answer += str(len(queue)) + queue[0]
+    
+        return answer
+    
+def solution(n):
+    result = look_and_say_sequence(n)
+    length = len(result)
+    
+    return result[length//2-1: length//2+1]
--- algorithm/tests.py
+++ algorithm/tests.py
@@ -1,9 +1,15 @@
-from look_and_say import look_and_say_sequence
+from look_and_say import solution
 import pytest
 
 @pytest.mark.parametrize("n, result", [
     (3, "21"),(4, "21"),(5, "12"),(6, "22"),(7, "12"),(8, "21"),
 ])
 def test_accuracy(n, result):
-    assert look_and_say_sequence(n) == result
-    
+    assert solution(n) == result
+  
+@pytest.mark.timeout(5)
+@pytest.mark.parametrize("n, result", [
+    (50, "**")
+])
+def test_efficiency(n, result):
+    assert solution(n) == result
