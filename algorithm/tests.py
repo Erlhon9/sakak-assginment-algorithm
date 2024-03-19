4df5a7385425929fccdd9659a230f914171f75fe
Erlhon9 <hongsbus@gmail.com>
2024-03-19 11:14:38 +0900

    fix: change result type to str

--- algorithm/tests.py
+++ algorithm/tests.py
@@ -2,7 +2,7 @@ from look_and_say import look_and_say_sequence
 import pytest
 
 @pytest.mark.parametrize("n, result", [
-    (3, 21),(4, 21),(5, 12),(6, 22),(7, 12),(8, 21),
+    (3, "21"),(4, "21"),(5, "12"),(6, "22"),(7, "12"),(8, "21"),
 ])
 def test_accuracy(n, result):
     assert look_and_say_sequence(n) == result
